742d4ae6cf2b8797f20cbcf20084ad0db4ff7c90
Taylor Swinton <taylorswinton1@gmail.com>
2019-02-19 23:28:31 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,5 @@
+require'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
@@ -7,20 +9,16 @@ class Shoe
   def initialize(brand)
     @brand = brand
     BRANDS << brand
-
-    arrayBrand = brand.split
-    arrayBrand = arrayBrand.uniq
-    
-      
-    end
-    
-    uniqueBrand = brand.uniq
-    BRANDS << uniqueBrand
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
+  end
+
+  def brand(brand)
+    @brand = brand
+    arrayBrand = brand.split
   end
 
 end
