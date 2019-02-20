ba51b72d12e2415795e29c3031d9a7676b7713d8
Taylor Swinton <taylorswinton1@gmail.com>
2019-02-20 00:00:59 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,22 +8,17 @@ class Shoe
     @brand = brand
     BRANDS << brand
 
-  end
-
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
-  end
-
-  def brand(brand)
-    @brand = brand
-
     brand.collect do |variable|
       if variable != brand[-1]
           BRANDS << variable
       end
     end
 
+  end
+
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
   end
 
 end
