11350512cb57ffc3fd27a5d760b77a63457ef0cf
Taylor Swinton <taylorswinton1@gmail.com>
2019-02-20 00:13:51 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,8 +5,8 @@ class Shoe
   BRANDS = []
 
   def initialize(brand)
-    @brand = brand
-    BRANDS << brand
+    #@brand = brand
+    #BRANDS << brand
 
     BRANDS.push(brand) unless BRANDS.include?(brand)
 
